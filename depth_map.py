701b0a440e6d0355b5ffcc59b4a5a3503fb3802d
ethanwaldie1996 <ethan.waldo2@gmail.com>
2016-12-22 23:27:03 -0500

    small changes

--- depth_map.py
+++ depth_map.py
@@ -3,10 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 cam = Kinect()
+cam2 = SimpleCV.Camera()
 
 
 while True:
     depth = cam.getDepth()
+    pic = cam2.getImage()
     data = depth.getNumpy()
+    pic.show()
 
     print data.shape
