3f98b631ecbaf7dbcbb312fcf26a21dd791f8072
ethanwaldie1996 <ethan.waldo2@gmail.com>
2016-12-28 11:28:53 -0500

    fixes

--- depth_map.py
+++ depth_map.py
@@ -8,8 +8,6 @@ bot = create2api.Create2()
 bot.start()
 bot.safe()
 
-plt.ion()
-
 X = 0
 Y = 0
 
@@ -17,27 +15,31 @@ phi = 0.089
 
 landmarks = {}
 
-
-for x in range(100):
+bot.drive_straight(100)
+for j in range(10):
     depth, timestamp = freenect.sync_get_depth()
+    print "Kinect Data Recived"
     data = depth[240]
     bot.get_packet(20)
 
     X = X + bot.sensor_state["distance"]
-
+    print "Processing"
     for i in range(len(data)):
+        print i
         if data[i] < 1900:
             angle = phi*(i-320)
 
             x_landmark = math.floor(data[i]*math.cos(angle)) + X
             y_landmark = math.floor(data[i]*math.sin(angle)) + Y
 
-            points = landmarks.keys()
-            x = [e[0] for e in points]
-            y = [e[1] for e in points]
-
-            plt.scattter(x,y)
-
             landmarks[(x_landmark, y_landmark)] = timestamp
 
-    bot.drive_straight(100)
+bot.drive_straight(0)
+points = landmarks.keys()
+x = [e[0] for e in points]
+y = [e[1] for e in points]
+
+plt.scattter(y,x)
+plt.show()
+
+bot.destory()
