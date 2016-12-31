1097770249a34ca397c5aeb65725d0a03a1fb4ac
ethanwaldie1996 <ethan.waldo2@gmail.com>
2016-12-31 16:02:06 -0500

    asdasdas

--- drive_direct.py
+++ drive_direct.py
@@ -5,12 +5,6 @@ import freenect
 import cv2
 import pygame
 
-#function to get RGB image from kinect
-def get_video():
-    array,_ = freenect.sync_get_video()
-    array = cv2.cvtColor(array,cv2.COLOR_RGB2BGR)
-    return array
-
 
 if __name__ == "__main__":
 
@@ -25,22 +19,22 @@ if __name__ == "__main__":
     pygame.init()
     screen = pygame.display.set_mode((30,30))
     pygame.display.set_caption('Pygame Keyboard Test')
-    pygame.mouse.set_visible(0)
 
     while 1:
         #frame = get_video()
         #cv2.imshow('RGB image',frame)
-        keys=pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
-            bot.drive_direct(100, 100)
-        elif keys[pygame.K_DOWN]:
-            bot.drive_direct(-100, -100)
-        elif keys[pygame.K_LEFT]:
-            bot.drive_direct(100, -100)
-        elif keys[pygame.K_LEFT]:
-            bot.drive_direct(-100, 100)
-        elif keys[pygame.K_ESCAPE]:
-            bot.drive_straight(0)
-            break
-
-    bot.destroy()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                bot.drive_straight(0)
+                bot.destroy()
+                sys.exit()
+            if event.type == pygame.K_UP:
+                bot.drive_direct(100, 100)
+            elif event.type == pygame.K_DOWN:
+                bot.drive_direct(-100, -100)
+            elif event.type == pygame.K_LEFT:
+                bot.drive_direct(100, -100)
+            elif event.type == pygame.K_RIGHT:
+                bot.drive_direct(-100, 100)
+            else:
+                bot.drive_straight(0)
