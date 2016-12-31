c3646f61df7c1813f1305560eb963f484b240347
ethanwaldie1996 <ethan.waldo2@gmail.com>
2016-12-31 16:07:54 -0500

    awdawd

--- drive_direct.py
+++ drive_direct.py
@@ -37,3 +37,5 @@ if __name__ == "__main__":
                     bot.drive_direct(100, -100)
                 elif event.key == pygame.K_RIGHT:
                     bot.drive_direct(-100, 100)
+                else:
+                    bot.drive_straight(0)
